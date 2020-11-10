517f48224b6dbc03fdb81b3f7ce8226837fe45bd
Kacper Gorgoń <kacper15011@gmail.com>
2020-11-10 15:55:03 +0100

    Update LoginInput

---
+++ QuicknoteMobileApp/Assets/Scripts/LoginInput.cs
@@ -0,0 +1,40 @@
+﻿/**
+  * \author: Kacper Gorgoń
+  * \date: 2020.11.10
+  * \brief: Login Input field functionality in LoggingScene
+  * v0.01
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Android;
+using UnityEngine.SceneManagement;
+
+public class LoginInput : MonoBehaviour
+{
+    private TouchScreenKeyboard keyboard;
+
+    public Text login;
+    // string loginText = "";
+
+    public void OpenKeyboard () 
+    {
+        keyboard = TouchScreenKeyboard.Open("Hello", TouchScreenKeyboardType.Default);
+    }
+
+    /*
+    void Update()
+    {
+        if(TouchScreenKeyboard.visible == false && keyboard != null)
+        {
+            if(keyboard.done)
+            {
+                loginText = login.text;
+                keyboard = null;
+            }
+        }
+    }
+    */
+}
--- QuicknoteMobileApp/Assets/Scripts/SignInButton.cs
+++ QuicknoteMobileApp/Assets/Scripts/SignInButton.cs
@@ -1,6 +1,14 @@
-﻿using System.Collections;
+﻿/**
+  * \author: Kacper Gorgoń
+  * \date: 2020.11.10
+  * \brief: Add User button functionality in LoggingScene
+  * v0.01
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Android;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +16,6 @@ public class SignInButton : MonoBehaviour
 {
     public void AddUser() 
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("SignUpScene");
     }
 }
