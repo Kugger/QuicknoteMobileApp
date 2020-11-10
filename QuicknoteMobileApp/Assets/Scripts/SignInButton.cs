49a0448c88a5c4b1bdfa0a1bd042e4d5151cd6d3
Kacper Gorgoń <kacper15011@gmail.com>
2020-11-10 12:59:07 +0100

    Add SignUp button functionality (Scene change)

---
+++ QuicknoteMobileApp/Assets/Scripts/SignInButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.SceneManagement;
+
+public class SignInButton : MonoBehaviour
+{
+    public void AddUser() 
+    {
+        SceneManager.LoadScene(1);
+    }
+}
