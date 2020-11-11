097c546169e268fefb07fcdbf85a1ce49f2c447e
Kacper Gorgoń <kacper15011@gmail.com>
2020-11-11 23:13:28 +0100

    Update LoggingSceneScript.cs
    Added basic functionality

--- QuicknoteMobileApp/Assets/Scripts/LoggingSceneScript.cs
+++ QuicknoteMobileApp/Assets/Scripts/LoggingSceneScript.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿/**
+  * \author: Kacper Gorgoń
+  * \date: 2020.11.11
+  * \brief: LoggingScene all actions in one script
+  * v0.01
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Android;
+using UnityEngine.SceneManagement;
 
 public class LoggingSceneScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TouchScreenKeyboard keyboard;
+
+    public Text login;
+
+    
+    public void LoginInput ()
     {
-        
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PasswordInput() 
     {
-        
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, true);
+    }
+
+    public void AddUser()
+    {
+        SceneManager.LoadScene("SignUpScene");
     }
 }
