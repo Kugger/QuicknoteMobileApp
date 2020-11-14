262d2aeb80d3be9e2960162f5e238e24e87d8f7f
Kacper Gorgoń <kacper15011@gmail.com>
2020-11-14 12:20:43 +0100

    Update DataTest.cs
    Added device's data and time

--- QuicknoteMobileApp/Assets/Scripts/DataTest.cs
+++ QuicknoteMobileApp/Assets/Scripts/DataTest.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DataTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text dataText;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        string time = System.DateTime.UtcNow.ToLocalTime().ToString("dd-MM-yyyy   HH:mm");
+
+        dataText.text = time;
     }
 }
