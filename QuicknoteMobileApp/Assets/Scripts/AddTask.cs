471b6721006811d9ff92dad888589c72cf9f48bf
Kacper Gorgoń <kacper15011@gmail.com>
2020-11-19 15:50:19 +0100

    Update AddTask script

--- QuicknoteMobileApp/Assets/Scripts/AddTask.cs
+++ QuicknoteMobileApp/Assets/Scripts/AddTask.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AddTask : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Dropdown category, type, year, month, day, colour;
+
+    public InputField descripiton;
+
+    public Button createButton;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(createButton == true)
+        {
+            Debug.Log(category);
+            Debug.Log(year);
+            Debug.Log(descripiton);
+        }
     }
 }
