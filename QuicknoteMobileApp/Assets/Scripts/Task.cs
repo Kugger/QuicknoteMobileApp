dc57a04157139a8c0eb2788be3691fe450715d35
Kacper Gorgoń <kacper15011@gmail.com>
2020-12-09 23:02:13 +0100

    Creating Task script as ScriptableObject and filling it

--- QuicknoteMobileApp/Assets/Scripts/Task.cs
+++ QuicknoteMobileApp/Assets/Scripts/Task.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Task", menuName = "Task")]
 public class Task : ScriptableObject
 {
+    public string sCategory;
+    public string sType;
+    public string sYear;
+    public string sMonth;
+    public string sDay;
+    public string sColour;
+    public string sDescription;
+    
 
     //  new branch test
 
